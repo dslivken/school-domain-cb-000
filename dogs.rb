208b96f53311355fbc3616f8f84a22ee5159aabe
David Slivken <slivken@gmail.com>
2018-06-21 09:25:03 +0000

    automatically backed up by learn

--- dogs.rb
+++ dogs.rb
@@ -22,17 +22,23 @@ howard=Dog.new("Howard")
 howard.owner="David"
 
 
-def count_sentences
-  nsent=[]
-  self.split(".").each do |s|
-    s.split("!").each do |f|
-      f.split("?").each do |l|
-        nsent<<l
-      end
-    end
-  end
-end
+# def count_sentences
+#   nsent=[]
+#   self.split(".").each do |s|
+#     s.split("!").each do |f|
+#       f.split("?").each do |l|
+#         nsent<<l
+#       end
+#     end
+#   end
+# end
 
+def count_s
+  self.split(".").each do |s|
+    nsent<< l
+  end
+  return nsent
+end
 
 sentence = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
 
